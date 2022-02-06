939f395c61bca6533796adc2b38e3f031840fa87
ChiYoPU <gossa192566@gmail.com>
2022-02-06 22:17:09 +0900

    コンポーネントを実装

--- 2minCordingPart11/v-for.js
+++ 2minCordingPart11/v-for.js
@@ -1,3 +1,15 @@
+Vue.component('tab1',{
+    template: '<div class="tab"><p>これはtab1のテストです</p></div>'
+});
+
+Vue.component('tab2',{
+    template: '<div class="tab"><p>これはtab2のテストです</p></div>'
+});
+
+Vue.component('tab3',{
+    template: '<div class="tab"><p>これはtab3のテストです</p></div>'
+});
+
 new Vue({
     el: '#app',
     data: {
