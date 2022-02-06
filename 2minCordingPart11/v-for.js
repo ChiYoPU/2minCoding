55702f262b5d92dfbf70f66abe24b7e4462d15e2
ChiYoPU <gossa192566@gmail.com>
2022-02-06 21:55:12 +0900

    表示するコンポーネントのオブジェクトを記載

---
+++ 2minCordingPart11/v-for.js
@@ -0,0 +1,11 @@
+new Vue({
+    el: '#app',
+    data: {
+        current: 'tab1',
+        tabs: {
+            'tab1': 'タブ1',
+            'tab2': 'タブ2',
+            'tab3': 'タブ3'
+        }
+    }
+})
